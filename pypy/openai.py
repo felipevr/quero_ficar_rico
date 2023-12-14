64b90e67ebeb86031e4dabf2b76b096dc13caf55
Felipe V. Rigo <github@felipevr.eti.br>
2023-12-13 22:09:26 -0400

    permite coletar e validar entrada de usuário

--- pypy/openai.py
+++ pypy/openai.py
@@ -1,6 +1,9 @@
 import random
 
 def gerar_grupos(n_grupos, n_numeros_por_grupo, min_numero, max_numero):
+    if not (0 < n_grupos <= (max_numero - min_numero + 1) // n_numeros_por_grupo):
+        raise ValueError("Número de grupos inválido")
+
     numeros_disponiveis = []
     grupos = []
 
@@ -23,7 +26,24 @@ def imprimir_grupos(grupos):
         grupo_formatado = [f'{num:02}' for num in grupo]
         print(f'Jogo {i}: {", ".join(grupo_formatado)}')
 
+
+def coletar_valores_entrada():
+    try:
+        n_grupos = int(input("Digite o número de grupos desejado: "))
+        n_numeros_por_grupo = int(input("Digite o número de números por grupo: "))
+        min_numero = int(input("Digite o número mínimo disponível: "))
+        max_numero = int(input("Digite o número máximo disponível: "))
+
+        grupos = gerar_grupos(n_grupos, n_numeros_por_grupo, min_numero, max_numero)
+        imprimir_grupos(grupos)
+        exit()
+
+    except ValueError as e:
+        print(f"Erro: {e}")
+
+
 if __name__ == "__main__":
+    coletar_valores_entrada()
     n_grupos = 5  # Altere o número de grupos conforme necessário
     n_numeros_por_grupo = 6
     min_numero = 1
