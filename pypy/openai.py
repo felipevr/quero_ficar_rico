316c7e1cdd684f9221fb5d31a4f5c6bc4c911582
Felipe V. Rigo <github@felipevr.eti.br>
2023-12-13 21:48:02 -0400

    ordena e melhora a formatação

--- pypy/openai.py
+++ pypy/openai.py
@@ -5,13 +5,15 @@ def gerar_grupos(n_grupos, n_numeros_por_grupo, min_numero, max_numero):
 
     for _ in range(n_grupos):
         grupo = random.sample(range(min_numero, max_numero + 1), n_numeros_por_grupo)
+        grupo.sort()  # Ordena os números em ordem crescente
         grupos.append(grupo)
 
     return grupos
 
 def imprimir_grupos(grupos):
     for i, grupo in enumerate(grupos, start=1):
-        print(f'Jogo {i}: {grupo}')
+        grupo_formatado = [f'{num:02}' for num in grupo]
+        print(f'Jogo {i}: {", ".join(grupo_formatado)}')
 
 if __name__ == "__main__":
     n_grupos = 5  # Altere o número de grupos conforme necessário
